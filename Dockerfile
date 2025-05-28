fe53e9e33f24ffb1346195687a6d8fca3894baa9
Nachiyappan Karuppiah <v-nkaruppiah@expediagroup.com>
2025-05-28 10:39:49 +0530

    jars

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM maven:3.9.6-eclipse-temurin-17 AS build
-
+# Use a lightweight JDK base image
 FROM openjdk:17-jdk-slim
 
+# Set working directory inside the container
 WORKDIR /app
 
-COPY target/cms-0.0.1-SNAPSHOT.jar app.jar
+# Copy the JAR file into the container
+COPY target/*.jar app.jar
 
-EXPOSE 8080
-
+# Run the application
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/shopping/cms/CmsApplication.java
+++ src/main/java/com/shopping/cms/CmsApplication.java
@@ -2,19 +2,12 @@ package com.shopping.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class CmsApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CmsApplication.class);
-	}
+public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
 	}
-
 }
